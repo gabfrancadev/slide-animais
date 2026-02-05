fc2f549a8b187588d18c525bde46c9e5451078c5
Gabriel França <gabfrancadev@gmail.com>
2026-02-05 15:39:21 -0300

    movement based o mouse events

---
+++ js/slide.js
@@ -0,0 +1,42 @@
+export default class Slide {
+  constructor(slide, wrapper) {
+    this.Slide = document.querySelector(slide);
+    this.wrapper = document.querySelector(wrapper);
+    this.dist = { finalPosition: 0, startX: 0, movement: 0 };
+  }
+  moveSlide(distX) {
+    this.dist.movePosition = distX;
+    this.Slide.style.transform = `translate3d(${distX}px, 0, 0)`;
+  }
+  uptadePosition(clientX) {
+    this.dist.movement = (this.dist.startX - clientX) * 1.6;
+    return this.dist.finalPosition - this.dist.movement;
+  }
+  onStart(event) {
+    event.preventDefault();
+    this.dist.startX = event.clientX;
+    this.wrapper.addEventListener("mousemove", this.onMove);
+  }
+  onMove(event) {
+    const finalPosition = this.uptadePosition(event.clientX);
+    this.Slide.style.transform = `translate3d(${finalPosition}px, 0, 0)`;
+  }
+  onEnd(event) {
+    this.wrapper.removeEventListener("mousemove", this.onMove);
+    this.dist.finalPosition = this.uptadePosition(event.clientX);
+  }
+  addSlideEvents() {
+    this.wrapper.addEventListener("mousedown", this.onStart);
+    this.wrapper.addEventListener("mouseup", this.onEnd);
+  }
+  bindEvents() {
+    this.onStart = this.onStart.bind(this);
+    this.onMove = this.onMove.bind(this);
+    this.onEnd = this.onEnd.bind(this);
+  }
+  init() {
+    this.bindEvents();
+    this.addSlideEvents();
+    return this;
+  }
+}
